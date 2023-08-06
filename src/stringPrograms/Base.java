7129c1b3d500d0d1cba78c5963c7388943353602
mahesh <shelkemahesh2893@gmail.com>
2023-08-06 23:20:18 +0530

    added few more string practice programs 1

--- src/stringPrograms/Base.java
+++ src/stringPrograms/Base.java
@@ -3,7 +3,7 @@ package stringPrograms;
 public class Base {
 	
 	static String b="Dream like Naruto, chase like fizzi";
-
+					//5     4    6       5     4    5
 	public static void main(String[] args) {
 		//  print the string
 		
